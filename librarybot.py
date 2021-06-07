fd88626836b57c2912f1d40180802923ad810396
Matthew Broadbent <matt@matthewbroadbent.net>
2021-06-07 21:06:14 +0100

    Make search more generic

--- librarybot.py
+++ librarybot.py
@@ -147,6 +147,7 @@ async def search(ctx, scope: str = 'all', attr: str = '*', value: str = '*'):
 	res = []
 	if attr == 'author':
 		attr = 'authors'
+	value.replace(" ", "_")
 	if scope == 'all':
 		if attr == '*':
 			res = cursor.execute("SELECT * FROM books ORDER BY title").fetchall()
